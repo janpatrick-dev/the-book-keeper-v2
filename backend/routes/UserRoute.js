9ece9a6c781084c95d6a3c256499765462f50a0f
janpatrick.dev <janpatrick.dev@gmail.com>
2023-07-29 20:06:21 +0800

    created user controller for user route

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,28 +1,10 @@
-const bcrypt = require('bcrypt');
 const userRouter = require('express').Router();
-const User = require('../models/User');
+const userController = require('../controllers/UserController');
 
 if (process.env.NODE_ENV === 'test') {
-  userRouter.get('/', async (request, response) => {
-    try {
-      const users = await User.find({});
-      response.status(200).json(users);
-    } catch (error) {
-      response.status(500).json({ error: error.message });
-    }
-  });
+  userRouter.get('/', userController.getAll);
 }
 
-userRouter.post('/', async (request, response) => {
-  try {
-    const body = request.body;
-    const saltRounds = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(body.password, saltRounds);
-    const newUser = await User.create({ ...body, password: hashedPassword });
-    response.status(201).json(newUser);
-  } catch (error) {
-    response.status(500).json({ error: error.message });
-  }
-});
+userRouter.post('/', userController.create);
 
 module.exports = userRouter;
